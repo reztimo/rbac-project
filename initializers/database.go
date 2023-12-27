35a2938f9fde0761ab0df5a58ad606d6d862c327
Maoelan <maulanamuhammad2000@gmail.com>
2023-12-27 22:48:40 +0700

    fix connect to database

--- initializers/database.go
+++ initializers/database.go
@@ -1,20 +1,17 @@
 package initializers
 
 import (
-	"os"
-
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func ConnectToDB() {
+/*func ConnectToDB() {
 	var err error
 	dsn := os.Getenv("DB_ULR")
 	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-
+		log.Fatal("Failed connect to database")
 	}
-}
+}*/
