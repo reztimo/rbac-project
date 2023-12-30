e094e73c93b1e014e2b9c97bc14f251eb6344d4f
Maoelan <maulanamuhammad2000@gmail.com>
2023-12-30 23:57:18 +0700

    add requireAuth & hasRequireROle

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"maoelana/RBAC-project/controllers"
 	"maoelana/RBAC-project/initializers"
+	"maoelana/RBAC-project/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,34 +17,34 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	// resource event
-	r.POST("/event", controllers.CreateResource)
-	r.GET("/event", controllers.GetAllResource)
-	r.GET("/event/:id", controllers.GetResource)
-	r.PUT("/event/:id", controllers.UpdateResource)
-	r.DELETE("/event/:id", controllers.DeleteResource)
+	// Endpoint-event
+	r.POST("/event", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.CreateResource)
+	r.GET("/event", middlewares.RequireAuth, controllers.GetAllResource)
+	r.GET("/event/:id", middlewares.RequireAuth, controllers.GetResource)
+	r.PUT("/event/:id", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.UpdateResource)
+	r.DELETE("/event/:id", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.DeleteResource)
 
-	// permission
-	r.POST("/permission", controllers.CreatePermission)
-	r.GET("/permission", controllers.GetAllPermission)
-	r.GET("/permission/:id", controllers.GetPermission)
-	r.PUT("/permission/:id", controllers.UpdatePermission)
-	r.DELETE("/permission/:id", controllers.DeletePermission) //warning
+	// Endpoint-permission
+	r.POST("/permission", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.CreatePermission)
+	r.GET("/permission", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.GetAllPermission)
+	r.GET("/permission/:id", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.GetPermission)
+	r.PUT("/permission/:id", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.UpdatePermission)
+	r.DELETE("/permission/:id", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.DeletePermission)
 
-	// role
-	r.POST("/role", controllers.CreateRole)
-	r.GET("/role", controllers.GetAllRole)
-	r.GET("/role/:id", controllers.GetRole)
-	r.PUT("/role/:id", controllers.EditRole)
-	r.DELETE("/role/:id", controllers.RemoveRole) //warning
+	// Endpoint-role
+	r.POST("/role", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.CreateRole)
+	r.GET("/role", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.GetAllRole)
+	r.GET("/role/:id", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.GetRole)
+	r.PUT("/role/:id", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.EditRole)
+	r.DELETE("/role/:id", middlewares.RequireAuth, middlewares.AuthorizationMiddleware("Admin"), controllers.RemoveRole)
 
-	// user
+	// Endpoint-user
 	r.POST("/register", controllers.Register)
 	r.POST("/login", controllers.Login)
-	r.GET("/validate", controllers.Validate)
-	r.GET("/profile", controllers.GetAllProfile)
-	r.GET("/profile/:id", controllers.GetProfile)
-	r.GET("/logout", controllers.Logout)
+	r.GET("/validate", middlewares.RequireAuth, controllers.Validate)
+	r.GET("/profile", middlewares.RequireAuth, controllers.GetAllProfile)
+	r.GET("/profile/:id", middlewares.RequireAuth, controllers.GetProfile)
+	r.GET("/logout", middlewares.RequireAuth, controllers.Logout)
 
 	r.Run()
 }
--- middlewares/authorizationMiddleware.go
+++ middlewares/authorizationMiddleware.go
@@ -1,1 +1,39 @@
 package middlewares
+
+import (
+	"maoelana/RBAC-project/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthorizationMiddleware(requireRole string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user, exists := c.Get("user")
+		if !exists {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		u, ok := user.(models.User)
+		if !ok {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		if hasRequireRole(u.Roles, requireRole) {
+			c.Next()
+		} else {
+			c.AbortWithStatus(http.StatusForbidden)
+		}
+	}
+}
+
+func hasRequireRole(roles []models.Role, requireRole string) bool {
+	for _, role := range roles {
+		if role.Name == requireRole {
+			return true
+		}
+	}
+	return false
+}
